650a9b3b1dd1027c60de95ea7b42ccc73010051a
WGT <williamgeorgethomas@gmail.com>
2022-11-30 20:52:17 -0600

    deleted commented out and unused code. started readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# weather-dashboard
+# WEATHER DASHBOARD
+
+# Link
+
+https://williamgeorgethomas.github.io/weather-dashboard/
+
+# Purpose
+
+XXXXXXXXXXXXXXXXXXXXXXXX
+This application was created to make a multiple choice quiz.  Users have only 30 seconds to take the quiz and each incorrect answer reduces the time by 5 seconds.  After completing the quiz, users can enter their name to record their score.  The top 5 scores are displayed when the user clicks the "VIEW HIGH SCORES" button.
+
+# My Experience
+
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+Building this application from scartch presented many challenges.  It required extensive use of JavaScript, a language I am still in the early stages of learning.  It took extensive testing, revisions, and help to complete the application.
+
+# Screenshot
+
+![screenshot of weather dashboard](XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX)
+
+# Credits
+
+Created by William George Thomas, 2022
--- script.js
+++ script.js
@@ -1,38 +1,31 @@
 //global variables
+
 let searchBtn = document.querySelector("#searchBtn");
 let inputEl = document.querySelector("#inputEl");
-let city1 = document.querySelector("#city1");
-let city2 = document.querySelector("#city2");
-let city3 = document.querySelector("#city3");
-let city4 = document.querySelector("#city4");
-let city5 = document.querySelector("#city5");
 let currentCityEl = document.querySelector("#currentCityEl");
 let currentTempEl = document.querySelector("#currentTempEl");
 let currentWindEl = document.querySelector("#currentWindEl");
 let currentHumidityEl = document.querySelector("#currentHumidityEl");
 let forecastEl = document.querySelector("#forecastEl");
-let day1El = document.querySelector("#day1El");
-let day2El = document.querySelector("#day2El");
-let day3El = document.querySelector("#day3El");
-let day4El = document.querySelector("#day4El");
-let day5El = document.querySelector("#day5El");
 let currentIconEl = document.querySelector("#currentIconEl");
 let searchedCities = JSON.parse(localStorage.getItem("cities")) || [];
 let searchHistoryButtons = document.querySelector("#searchHistoryButtons");
-
-//divs on side of html
+let forecastTitle = document.querySelector("#forecastTitle");
+let cityCard = document.querySelector("#cityCard");
 
 //functions
+
 function init() {
-  //grab last search results from local storage and put on left side of page
+  //grabs last search results from local storage and put on left side of page
 
   if (searchedCities.length > 0) {
     let maximumLenght = searchedCities.length >= 5 ? 5 : searchedCities.length;
     let count = 0;
     for (let i = searchedCities.length - 1; i >= 0; i--) {
-      let cityHistoryButtons = document.createElement("div");
-      cityHistoryButtons.innerHTML += `<button>${searchedCities[i]}</button>`;
-      cityHistoryButtons.classList.add("w-full", "border-4", "border-black", "rounded-lg", "my-2", "text-center");
+      let cityHistoryButtons = document.createElement("button");
+      cityHistoryButtons.setAttribute("data-city", searchedCities[i]);
+      cityHistoryButtons.innerHTML = searchedCities[i];
+      cityHistoryButtons.classList.add("w-full", "border-4", "border-black", "rounded-lg", "my-2", "text-center", "hover:bg-gray-300");
       searchHistoryButtons.append(cityHistoryButtons);
       count++;
       if (count === maximumLenght) {
@@ -49,105 +42,80 @@ let formSubmitHandler = function (event) {
   search();
 };
 
-
 let buttonClickHandler = function (event) {
-let cityChoice = event.target.value;
-if (cityChoice) {
-  search();
-//how do i get the serach to specify the input without using the input box???
-}
+  let parentDiv = event.target;
+  let cityChoice = parentDiv.getAttribute("data-city");
+  if (cityChoice) {
+    search(cityChoice);
+  }
+};
 
-
-}
-
-
-
-function search() {
-  let city = inputEl.value;
+function search(cityChoice) {
+  let city = inputEl.value || cityChoice;
   let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=6c722ab2da2f45aa2671601003f6488c&units=imperial`;
   let cityHistory = inputEl.value.trim();
-  if (cityHistory === "") {
-    return;
-  }
 
   //clears search box after each time
   inputEl.value = "";
 
   forecastEl.innerHTML = "";
+  currentCityEl.innerHTML = "";
 
-  if (!searchedCities.find((city) => city.toLowerCase() === cityHistory.toLowerCase())) {
+  if (!searchedCities.find((city) => city.toLowerCase() === cityHistory.toLowerCase()) && cityHistory !== "") {
     searchedCities.push(cityHistory);
+    localStorage.setItem("cities", JSON.stringify(searchedCities));
   }
-
-  localStorage.setItem("cities", JSON.stringify(searchedCities));
 
   fetch(requestUrl)
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      console.log(data.city.name);
-      console.log(data.list[0].main.temp);
-      console.log(data.list[0].wind.speed);
-      console.log(data.list[0].main.humidity);
-      console.log(data.list[0].weather[0].icon);
-
-      //code goes here
-
       //city name, temp, wind, humidity
 
-      // let iconCode = data.list[0].weather[0].icon;
-      // let iconURL = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
-      let date = data.list[0].dt_txt.split(" ")[0];
+      let date = dayjs(data.list[0].dt_txt).format("MMMM DD, YYYY");
 
-      currentCityEl.innerHTML += `${data.city.name} (${date})`;
-      currentTempEl.innerHTML += `${data.list[0].main.temp}° F`;
-      currentWindEl.innerHTML += `${data.list[0].wind.speed} MPH`;
-      currentHumidityEl.innerHTML += `${data.list[0].main.humidity}%`;
+      currentCityEl.innerHTML = `${data.city.name} (${date})`;
+      currentTempEl.innerHTML = `Temp: ${data.list[0].main.temp}° F`;
+      currentWindEl.innerHTML = `Wind: ${data.list[0].wind.speed} MPH`;
+      currentHumidityEl.innerHTML = `Humidity ${data.list[0].main.humidity}%`;
+
+      let iconURL = `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
+      let weatherIcon = document.createElement("img");
+      weatherIcon.setAttribute("src", iconURL);
+      currentCityEl.append(weatherIcon);
 
       //5 day forecast
+
+      cityCard.classList.remove("hidden");
+      forecastTitle.innerHTML = "5 DAY FORECAST";
+
       data.list.forEach((day) => {
         let midnight = day.dt_txt.split(" ")[1];
-        let date = day.dt_txt.split(" ")[0];
+        let date = dayjs(day.dt_txt).format("MMMM DD, YYYY");
         if (midnight === "00:00:00") {
-          console.log(day);
           let dayCardEl = document.createElement("div");
-          let dateEl = document.createElement("h3");
-          let tempEl = document.createElement("p");
-          let windEl = document.createElement("p");
-          let humidityEl = document.createElement("p");
-          dayCardEl.innerHTML += `<h1>${date}</h1>`;
+          dayCardEl.innerHTML += `<h1 class="font-bold">${date}</h1>`;
+          dayCardEl.innerHTML += `<img src="${iconURL}">`;
           dayCardEl.innerHTML += `<div>Temp: ${day.main.temp}°</div>`;
           dayCardEl.innerHTML += `<div>Wind: ${day.wind.speed} MPH</div>`;
           dayCardEl.innerHTML += `<div>Humidity ${day.main.humidity}%</div>`;
-          dayCardEl.classList.add("border-4", "border-black", "rounded-lg", "p-5", "m-2", "w-full", "md:w-3/12");
+          dayCardEl.classList.add("border-4", "border-black", "rounded-lg", "p-2", "m-2", "bg-gradient-to-r", "from-cyan-500", "to-blue-500", "text-white");
           forecastEl.append(dayCardEl);
-          // dayCardEl.append(dateEl);
-          // dayCardEl.append(tempEl);
-          // dayCardEl.append(windEl);
-          // dayCardEl.append(humidityEl);
         }
       });
-
-      // day1El.innerHTML = data.list[1].main.temp;
-      // day2El.innerHTML = data.list[2].main.temp;
-      // day3El.innerHTML = data.list[3].main.temp;
-      // day4El.innerHTML = data.list[4].main.temp;
-      // day5El.innerHTML = data.list[5].main.temp;
     });
 }
 
 //function calls
 
-//event listeners
 init();
+
+//event listeners
 
 //search button event listener
 searchBtn.addEventListener("click", formSubmitHandler);
 
-//past search button event listener
-searchHistoryButtons.addEventListener('click', buttonClickHandler);
-
-//click on past results
+//search history button event listener
+searchHistoryButtons.addEventListener("click", buttonClickHandler);
